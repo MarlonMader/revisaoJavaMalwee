f3cd77eaa97858f09da940f9743ae86274f4318d
MarlonMader <leonardorafaelli@gmail.com>
2022-03-04 16:26:08 -0300

    Projeto de revisão Java

---
+++ src/br/com/senai/controller/DadosController.java
@@ -0,0 +1,16 @@
+package br.com.senai.controller;
+
+public class DadosController {
+    public void cadastrarPessoa(){
+        System.out.print("Cadastro de Pessoas");
+    }
+    public void listarPessoa(){
+        System.out.print("Pessoas Cadastradas");
+    }
+    public void editarPessoa(){
+        System.out.print("Editar dados de Pessoas");
+    }
+    public void deletarPessoa(){
+        System.out.print("Excluir Pessoas");
+    }
+}
